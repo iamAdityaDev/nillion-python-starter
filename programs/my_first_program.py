481d028e4e5ba23dfe5153c0f4f1aecae9916f06
iamAdityaDev <iamadityadevpro@gmail.com>
2024-06-26 05:07:48 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -6,7 +6,8 @@ def nada_main():
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Perform the addition of my_int1 and my_int2
+    sum_int = my_int1 + my_int2
 
-    return [Output(my_int1, "my_output", party1)]
+    # Return the result of the addition
+    return [Output(sum_int, "my_output", party1)]
