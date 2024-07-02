f899d32e28f8be8030e8729c15ec385a8b691378
Aditya Dev <107423108+iamAdityaDev@users.noreply.github.com>
2024-07-02 13:05:49 +0530

    Update my_first_program.py

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,13 +1,87 @@
 from nada_dsl import *
-
+from typing import List
 
 def nada_main():
     party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    party2 = Party(name="Party2")
+    
+    # Inputs for party1
+    plaintext1 = SecretString(Input(name="plaintext1", party=party1))
+    key1 = SecretString(Input(name="key1", party=party1))
 
-    # Perform the addition of my_int1 and my_int2
-    sum_int = my_int1 + my_int2
+    # Inputs for party2
+    plaintext2 = SecretString(Input(name="plaintext2", party=party2))
+    key2 = SecretString(Input(name="key2", party=party2))
 
-    # Return the result of the addition
-    return [Output(sum_int, "my_output", party1)]
+    # Encrypt the plaintexts with the keys
+    encrypted1 = encrypt_aes(plaintext1, key1)
+    encrypted2 = encrypt_aes(plaintext2, key2)
+
+    # Decrypt the encrypted texts
+    decrypted1 = decrypt_aes(encrypted1, key1)
+    decrypted2 = decrypt_aes(encrypted2, key2)
+    
+    # Perform XOR operation on the decrypted texts
+    xor_result = xor_strings(decrypted1, decrypted2)
+    
+    # Encrypt the XOR result with a new key
+    final_key = SecretString(Input(name="final_key", party=party1))
+    final_encryption = encrypt_aes(xor_result, final_key)
+
+    # Return the final encrypted result
+    return [Output(final_encryption, "final_output", party1)]
+
+def encrypt_aes(plaintext: SecretString, key: SecretString) -> SecretString:
+    """
+    A mock function to represent AES encryption.
+    """
+    # Mock implementation
+    return SecretString(plaintext + "_encrypted_with_" + key)
+
+def decrypt_aes(ciphertext: SecretString, key: SecretString) -> SecretString:
+    """
+    A mock function to represent AES decryption.
+    """
+    # Mock implementation
+    return SecretString(ciphertext.replace("_encrypted_with_" + key, ""))
+
+def xor_strings(s1: SecretString, s2: SecretString) -> SecretString:
+    """
+    Perform XOR operation on two strings.
+    """
+    return SecretString("".join(chr(ord(a) ^ ord(b)) for a, b in zip(s1, s2)))
+
+# Test the functions with the nada_main setup
+if __name__ == "__main__":
+    print(nada_main())
+
+# Definitions for SecretString and Input/Output classes are assumed as follows:
+class SecretString:
+    def __init__(self, value: str):
+        self.value = value
+    
+    def __add__(self, other):
+        if isinstance(other, SecretString):
+            return SecretString(self.value + other.value)
+        return SecretString(self.value + other)
+    
+    def replace(self, old, new):
+        return SecretString(self.value.replace(old, new))
+    
+    def __str__(self):
+        return self.value
+
+class Input:
+    def __init__(self, name: str, party: Party):
+        self.name = name
+        self.party = party
+
+class Output:
+    def __init__(self, value: SecretString, name: str, party: Party):
+        self.value = value
+        self.name = name
+        self.party = party
+
+class Party:
+    def __init__(self, name: str):
+        self.name = name
